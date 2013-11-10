1c88108a756b38c30c4cf9566c06b3833c7ded7f
U-Fish-PC\Daniel <corrnondacqb@yahoo.com>
2013-11-10 13:12:44 -0500

    Update Route 2

--- main.asm
+++ main.asm
@@ -86153,31 +86153,31 @@ Route2_h: ; 54000 (15:4000)
 	dw Route2Object ;Object Data Pointer
 
 Route2Object: ; 0x54022 (size=72)
-	db $f ; border tile
+	db $43 ; border tile
 
 	db $6 ; warps
-	db $9, $c, $0, DIGLETTS_CAVE_EXIT
-	db $b, $3, $1, VIRIDIAN_FOREST_EXIT
-	db $13, $f, $0, ROUTE_2_HOUSE
-	db $23, $10, $1, ROUTE_2_GATE
-	db $27, $f, $2, ROUTE_2_GATE
-	db $2b, $3, $2, VIRIDIAN_FOREST_ENTRANCE
+	db $5, $e, $0, DIGLETTS_CAVE_EXIT
+	db $7, $5, $1, VIRIDIAN_FOREST_EXIT
+	db $29, $7, $0, ROUTE_2_HOUSE
+	db $15, $c, $1, ROUTE_2_GATE
+	db $19, $b, $2, ROUTE_2_GATE
+	db $21, $5, $2, VIRIDIAN_FOREST_ENTRANCE
 
 	db $2 ; signs
-	db $41, $5, $3 ; Route2Text3
-	db $b, $b, $4 ; Route2Text4
+	db $37, $7, $3 ; Route2Text3
+	db $7, $d, $4 ; Route2Text4
 
 	db $2 ; people
-	db SPRITE_BALL, $36 + 4, $d + 4, $ff, $ff, $81, MOON_STONE ; item
-	db SPRITE_BALL, $2d + 4, $d + 4, $ff, $ff, $82, HP_UP ; item
+	db SPRITE_BALL, $2b + 4, $6 + 4, $ff, $ff, $81, MOON_STONE ; item
+	db SPRITE_BALL, $25 + 4, $11 + 4, $ff, $ff, $82, HP_UP ; item
 
 	; warp-to
-	EVENT_DISP $a, $9, $c ; DIGLETTS_CAVE_EXIT
-	EVENT_DISP $a, $b, $3 ; VIRIDIAN_FOREST_EXIT
-	EVENT_DISP $a, $13, $f ; ROUTE_2_HOUSE
-	EVENT_DISP $a, $23, $10 ; ROUTE_2_GATE
-	EVENT_DISP $a, $27, $f ; ROUTE_2_GATE
-	EVENT_DISP $a, $2b, $3 ; VIRIDIAN_FOREST_ENTRANCE
+	EVENT_DISP $a, $5, $e ; DIGLETTS_CAVE_EXIT
+	EVENT_DISP $a, $7, $5 ; VIRIDIAN_FOREST_EXIT
+	EVENT_DISP $a, $29, $7 ; ROUTE_2_HOUSE
+	EVENT_DISP $a, $15, $c ; ROUTE_2_GATE
+	EVENT_DISP $a, $19, $b ; ROUTE_2_GATE
+	EVENT_DISP $a, $21, $5 ; VIRIDIAN_FOREST_ENTRANCE
 
 INCBIN "baserom.gbc",$5406a,$5407e - $5406a
 
