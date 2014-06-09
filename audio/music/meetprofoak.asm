65ca8aff71399ee4b2ceb9ef0d01cfc39e09ca72
U-Fish-PC\Daniel <corrnondacqb@yahoo.com>
2014-06-09 09:55:32 -0400

    Update MeetProfOak

--- audio/music/meetprofoak.asm
+++ audio/music/meetprofoak.asm
@@ -1,162 +1,155 @@
-Music_MeetProfOak_Ch1:: ; af59 (2:6f59)
-	tempo 0, 112
+Music_MeetProfOak_Ch1::
+	tempo 0, 118
 	stereopanning 119
+	vibrato 18, 3, 4
 	duty 3
-	togglecall
-	notetype 12, 11, 4
-	octave 3
+	notetype 12, 9, 8
+	octave 2
 	note F#, 1
 	note B_, 1
-	octave 4
-	note D#, 1
-	note E_, 1
-	note F#, 12
-	notetype 12, 10, 2
 	octave 3
-	note E_, 6
-	note B_, 10
-	note E_, 6
-	note B_, 10
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
-	note E_, 6
-	note B_, 10
-	note E_, 6
-	note B_, 10
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
+	note D#, 1
+	note F#, 1
+	note A_, 16
+	note G#, 8
+	notetype 12, 9, 7
+	note G#, 8
+	notetype 12, 9, 1
 
-Music_MeetProfOak_branch_af85::
-	note F#, 6
-	note E_, 4
+Music_MeetProfOak_Ch1_branch_1::
+	callchannel Music_MeetProfOak_Ch1_branch_3
+	callchannel Music_MeetProfOak_Ch1_branch_3
+	callchannel Music_MeetProfOak_Ch1_branch_4
+	callchannel Music_MeetProfOak_Ch1_branch_4
+	loopchannel 2, Music_MeetProfOak_Ch1_branch_1
+
+Music_MeetProfOak_Ch1_branch_2::
+	callchannel Music_MeetProfOak_Ch1_branch_5
+	callchannel Music_MeetProfOak_Ch1_branch_5
+	callchannel Music_MeetProfOak_Ch1_branch_6
+	callchannel Music_MeetProfOak_Ch1_branch_6
+	callchannel Music_MeetProfOak_Ch1_branch_5
+	callchannel Music_MeetProfOak_Ch1_branch_5
+	callchannel Music_MeetProfOak_Ch1_branch_6
+	callchannel Music_MeetProfOak_Ch1_branch_6
+	loopchannel 0, Music_MeetProfOak_Ch1_branch_2
+
+Music_MeetProfOak_Ch1_branch_3::
+	octave 2
+	note B_, 2
+	octave 3
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note B_, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	endchannel
+
+Music_MeetProfOak_Ch1_branch_4::
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	note F#, 2
+	note D#, 2
+	note B_, 2
+	note D#, 2
+	note F#, 2
+	note D#, 2
+	endchannel
+
+Music_MeetProfOak_Ch1_branch_5::
+	octave 2
 	note A_, 2
-	note A_, 4
-	note F#, 6
-	note E_, 4
+	octave 3
+	note C#, 2
+	note E_, 2
+	note C#, 1
+	note E_, 1
+	note C#, 2
+	notetype 12, 9, 2
 	note A_, 2
-	note A_, 4
-	note F#, 6
-	note E_, 4
-	note G#, 2
+	notetype 12, 9, 1
+	note C#, 2
+	notetype 12, 9, 2
+	note E_, 2
+	notetype 12, 9, 1
+	endchannel
+
+Music_MeetProfOak_Ch1_branch_6::
+	octave 2
+	note B_, 1
+	octave 3
+	note E_, 1
+	notetype 12, 9, 2
 	note G#, 4
-	note F#, 6
 	note E_, 4
-	note G#, 2
-	note G#, 4
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
-	note F#, 6
-	note E_, 4
-	note G#, 2
-	note G#, 4
-	note F#, 6
-	note E_, 4
-	note G#, 2
-	note G#, 4
-	loopchannel 0, Music_MeetProfOak_branch_af85
+	notetype 12, 9, 1
+	note E_, 1
+	note G#, 1
+	notetype 12, 9, 2
+	note B_, 4
+	notetype 12, 9, 1
+	endchannel
 
 
-Music_MeetProfOak_Ch2:: ; afa9 (2:6fa9)
-	vibrato 8, 1, 1
-	duty 2
-	notetype 12, 12, 4
+Music_MeetProfOak_Ch2::
+	vibrato 18, 3, 4
+	duty 3
+	notetype 12, 10, 8
 	octave 3
 	note B_, 1
 	octave 4
 	note D#, 1
 	note F#, 1
 	note A#, 1
-	note B_, 12
-	notetype 12, 11, 2
-	octave 3
-	note B_, 2
-	octave 4
-	note C#, 2
-	note D#, 2
-	note E_, 4
-	note D#, 2
-	note C#, 4
-	notetype 12, 6, 4
-	octave 4
-	note B_, 2
-	octave 5
-	note C#, 2
-	note D#, 2
-	note E_, 4
-	note D#, 2
-	note C#, 4
-	notetype 12, 11, 2
-	octave 3
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 4
-	note B_, 2
-	note B_, 4
-	notetype 12, 8, 1
-	octave 4
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 4
-	note B_, 2
-	note B_, 4
-	notetype 12, 11, 2
-	octave 3
-	note B_, 2
-	octave 4
-	note C#, 2
-	note D#, 2
-	note E_, 4
-	note D#, 2
-	note C#, 4
-	notetype 12, 6, 4
-	octave 4
-	note B_, 2
-	octave 5
-	note C#, 2
-	note D#, 2
-	note E_, 4
-	note D#, 2
-	note C#, 4
-	notetype 12, 11, 2
-	octave 3
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 4
-	note B_, 2
-	note B_, 4
-	notetype 12, 8, 1
-	octave 4
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 4
-	note B_, 2
-	note B_, 4
-	notetype 12, 11, 5
+	note B_, 16
+	notetype 12, 10, 7
+	note B_, 10
+	duty 2
 
-Music_MeetProfOak_branch_b005::
+Music_MeetProfOak_Ch2_branch_1::
+	notetype 12, 11, 2
+	octave 3
+	note B_, 2
 	octave 4
+	note C#, 2
+	note D#, 2
+	note E_, 4
+	note D#, 2
+	note C#, 4
+	notetype 12, 9, 1
+	note B_, 2
+	octave 5
+	note C#, 2
+	note D#, 2
+	note E_, 4
+	note D#, 2
+	note C#, 4
+	notetype 12, 11, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note A_, 4
+	note B_, 1
+	rest 1
+	note B_, 4
+	notetype 12, 9, 1
+	octave 4
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note A_, 4
+	note B_, 2
+	note B_, 4
+	loopchannel 2, Music_MeetProfOak_Ch2_branch_1
+	rest 6
+	notetype 12, 11, 4
+
+Music_MeetProfOak_Ch2_branch_2::
 	note C#, 6
 	octave 3
 	note A_, 1
@@ -175,14 +168,14 @@ Music_MeetProfOak_branch_b005::
 	note B_, 1
 	octave 4
 	note E_, 8
-	notetype 12, 6, 5
-	octave 4
+	notetype 12, 8, 2
 	note B_, 6
 	note G#, 1
 	note B_, 1
+	notetype 12, 8, 3
 	octave 5
 	note E_, 8
-	notetype 12, 11, 5
+	notetype 12, 11, 4
 	octave 3
 	note A_, 6
 	note F#, 1
@@ -203,222 +196,112 @@ Music_MeetProfOak_branch_b005::
 	note B_, 1
 	octave 4
 	note E_, 1
-	notetype 12, 11, 7
 	note G#, 16
-	loopchannel 0, Music_MeetProfOak_branch_b005
+	loopchannel 0, Music_MeetProfOak_Ch2_branch_2
 
 
-Music_MeetProfOak_Ch3:: ; b03d (2:703d)
-	notetype 12, 1, 2
-	rest 10
+Music_MeetProfOak_Ch3::
+	notetype 12, 1, 4
+	rest 4
+	octave 3
+	note E_, 2
+	rest 2
 	octave 4
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note B_, 1
-	rest 5
-	octave 5
-	note E_, 4
-	rest 6
-	octave 4
-	note B_, 1
-	rest 5
-	octave 5
-	note E_, 4
-	octave 4
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note F#, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
 	note E_, 1
 	rest 1
+	octave 3
+	note A_, 2
+	rest 2
+	octave 2
+	note B_, 2
+	octave 3
 	note B_, 1
 	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note G_, 1
-	rest 1
-	note G#, 1
-	rest 1
+	note E_, 2
+	callchannel Music_MeetProfOak_Ch3_branch_3
 
-Music_MeetProfOak_branch_b0bc::
-	octave 4
-	note A_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note A_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note A_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note A_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	loopchannel 2, Music_MeetProfOak_branch_b0bc
+Music_MeetProfOak_Ch3_branch_1::
+	callchannel Music_MeetProfOak_Ch3_branch_3
+	callchannel Music_MeetProfOak_Ch3_branch_3
+	callchannel Music_MeetProfOak_Ch3_branch_4
+	callchannel Music_MeetProfOak_Ch3_branch_4
+	loopchannel 2, Music_MeetProfOak_Ch3_branch_1
 
-Music_MeetProfOak_branch_b0d8::
+Music_MeetProfOak_Ch3_branch_2::
+	callchannel Music_MeetProfOak_Ch3_branch_5
+	callchannel Music_MeetProfOak_Ch3_branch_5
+	callchannel Music_MeetProfOak_Ch3_branch_6
+	callchannel Music_MeetProfOak_Ch3_branch_6
+	callchannel Music_MeetProfOak_Ch3_branch_5
+	callchannel Music_MeetProfOak_Ch3_branch_5
+	callchannel Music_MeetProfOak_Ch3_branch_6
+	callchannel Music_MeetProfOak_Ch3_branch_6
+	loopchannel 0, Music_MeetProfOak_Ch3_branch_2
+
+Music_MeetProfOak_Ch3_branch_3::
+	note E_, 2
+	rest 2
 	octave 4
-	note G#, 1
+	note E_, 1
 	rest 1
+	octave 3
+	note G#, 2
+	rest 2
+	octave 2
+	note B_, 2
+	octave 3
 	note B_, 1
 	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	loopchannel 2, Music_MeetProfOak_branch_b0d8
+	note E_, 2
+	endchannel
 
-Music_MeetProfOak_branch_b0ed::
-	note F#, 1
+Music_MeetProfOak_Ch3_branch_4::
+	octave 2
+	note B_, 2
+	rest 2
+	octave 4
+	note D#, 1
 	rest 1
-	note A_, 1
+	octave 3
+	note D#, 2
+	rest 2
+	note F#, 2
+	note B_, 1
 	rest 1
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	loopchannel 2, Music_MeetProfOak_branch_b0ed
+	note D#, 2
+	endchannel
 
-Music_MeetProfOak_branch_b101::
+Music_MeetProfOak_Ch3_branch_5::
+	octave 2
+	note A_, 2
+	rest 2
+	octave 3
+	note A_, 1
+	rest 1
+	octave 2
+	note A_, 2
+	rest 2
+	octave 3
+	note E_, 2
+	note A_, 1
+	rest 1
+	note C#, 2
+	endchannel
+
+Music_MeetProfOak_Ch3_branch_6::
+	octave 2
+	note G#, 2
+	rest 2
+	octave 3
 	note G#, 1
 	rest 1
-	note B_, 1
-	rest 1
+	octave 2
+	note G#, 2
+	rest 2
+	octave 3
+	note E_, 2
 	note G#, 1
 	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	loopchannel 2, Music_MeetProfOak_branch_b101
-	loopchannel 0, Music_MeetProfOak_branch_b0bc
-; 0xb119
+	octave 2
+	note B_, 2
+	endchannel
