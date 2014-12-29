485c1466655794be6b14474c83ca8c7df0eae859
U-Daniel-Laptop\Daniel <corrnondacqb@yahoo.com>
2014-12-29 11:36:29 -0500

    Route 1 text

--- text/maps/route_1.asm
+++ text/maps/route_1.asm
@@ -1,15 +1,12 @@
 _Route1ViridianMartSampleText:: ; 8d5bf (23:55bf)
-	text "Hi! I work at a"
-	line "#mon Mart."
+	text "I found this"
+	line "Potion just"
+	cont "laying around."
 
-	para "It's a convenient"
-	line "shop, so please"
-	cont "visit us in"
-	cont "Viridian City."
-
-	para "I know, I'll give"
-	line "you a sample!"
-	cont "Here you go!"
+	para "I think you could"
+	line "find a better use"
+	cont "for it than I"
+	cont "can."
 	prompt
 
 _Route1Text_1cae8:: ; 8d643 (23:5643)
@@ -19,9 +16,8 @@ _Route1Text_1cae8:: ; 8d643 (23:5643)
 	text "!@@"
 
 _Route1Text_1caee:: ; 8d652 (23:5652)
-	text "We also carry"
-	line "# Balls for"
-	cont "catching #mon!"
+	text "Good luck on your"
+	line "adventure!"
 	done
 
 _Route1Text_1caf3:: ; 8d67c (23:567c)
@@ -30,16 +26,11 @@ _Route1Text_1caf3:: ; 8d67c (23:567c)
 	done
 
 _Route1Text2:: ; 8d69f (23:569f)
-	text "See those ledges"
-	line "along the road?"
-
-	para "It's a bit scary,"
-	line "but you can jump"
-	cont "from them."
-
-	para "You can get back"
-	line "to Pallet Town"
-	cont "quicker that way."
+	text "You can jump over"
+	line "the ledges but"
+	cont "once you go over"
+	cont "them, there's no"
+	cont "turning back."
 	done
 
 _Route1Text3:: ; 8d720 (23:5720)
