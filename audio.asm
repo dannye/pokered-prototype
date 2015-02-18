dannye/pokered-prototype

commit a907ffc1d9e230e01020c737be9b5b233ed5dc6a

Build only one version

main.asm
@@ -1,4 +1,5 @@
 INCLUDE "constants.asm"
+INCLUDE "charmap.asm"
 
 NPC_SPRITES_1 EQU $4
 NPC_SPRITES_2 EQU $5
@@ -6331,13 +6332,7 @@ SECTION "bank1A",ROMX,BANK[$1A]
 
 INCLUDE "engine/battle/1a.asm"
 
-Version_GFX:
-IF DEF(_RED)
-	INCBIN "gfx/red/redgreenversion.1bpp" ; 10 tiles
-ENDC
-IF DEF(_BLUE)
-	INCBIN "gfx/blue/blueversion.1bpp" ; 8 tiles
-ENDC
+Version_GFX:       INCBIN "gfx/prototypeversion.1bpp" ; 10 tiles
 
 Dojo_GFX:
 Gym_GFX:           INCBIN "gfx/tilesets/gym.2bpp"

prototype/audio.asm
@@ -1,2 +1,1 @@
-INCLUDE "blue.asm"
 INCLUDE "audio.asm"

prototype/main.asm
@@ -1,2 +1,1 @@
-INCLUDE "blue.asm"
 INCLUDE "main.asm"

red/audio.asm
@@ -1,2 +1,0 @@
-INCLUDE "red.asm"
-INCLUDE "audio.asm"

red/main.asm
@@ -1,2 +1,0 @@
-INCLUDE "red.asm"
-INCLUDE "main.asm"

yellow/audio.asm
@@ -1,2 +1,0 @@
-INCLUDE "yellow.asm"
-INCLUDE "audio.asm"

yellow/main.asm
@@ -1,2 +1,0 @@
-INCLUDE "yellow.asm"
-INCLUDE "main.asm"

commit cb96a8a90b74dbdc78459f38095699bdb2cfe90f

Fix PC broken by https://github.com/dannye/pokered-prototype/commit/a907ffc1d9e230e01020c737be9b5b233ed5dc6a bills_pc.asm didnt't have charmap.asm

audio.asm
@@ -515,8 +515,6 @@ Unknown_213c4: ; 213c4 (8:53c4)
 	db $00,$00,$00,$80
 
 
-INCLUDE "engine/menu/bills_pc.asm"
-
 INCLUDE "audio/engine_2.asm"
 
 

main.asm
@@ -5072,6 +5072,8 @@ INCLUDE "engine/menu/oaks_pc.asm"
 
 INCLUDE "engine/hidden_object_functions7.asm"
 
+INCLUDE "engine/menu/bills_pc.asm"
+
 
 SECTION "Pics 1", ROMX, BANK[PICS_1]
 